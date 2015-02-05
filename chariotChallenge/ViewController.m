354614a7d77a471d5006bdc74442d17ec2179d47
Luke Solomon <luke@makeschool.com>
2015-02-04 16:37:12 -0800

    Storyboard mostly finished. Starting work on server connections. Setting up heroku

--- chariotChallenge/ViewController.m
+++ chariotChallenge/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)buttonLogIn:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldEmail;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPassword;
 
 @end
 
@@ -23,5 +26,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)buttonLogIn:(UIButton *)sender {
+    NSString *post = [NSString stringWithFormat: @"&Username=%@ &Password=%@", @"username", @"password"];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.abcde.com/xyz/login.aspx"]]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
+    [request setHTTPBody:postData];
+     NSURLConnection *conn = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+     if (conn){
+         NSLog(@"Connection Successful");
+     } else {
+         NSLog(@"Connection Failed");
+     }
+}
+
+
 
 @end
