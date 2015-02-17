b240dfe0a0cbf8c263739b1141ac647c8575fb17
Luke Solomon <luke@makeschool.com>
2015-02-17 15:38:59 -0800

    get request almost done

--- chariotChallenge/UserProfileViewController.m
+++ chariotChallenge/UserProfileViewController.m
@@ -10,26 +10,39 @@
 
 @interface UserProfileViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textFieldFirst;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldLast;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldEmail;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPhone;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldCurrentPassword;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldNewPassword;
+@property (weak, nonatomic) IBOutlet UIButton *buttonUpdate;
+
+
 @end
 
-@implementation UserProfileViewController
+@implementation UserProfileViewController {
+    NSDictionary *userProfileData;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self serverGetRequest];
+    
+    self.textFieldFirst.text = userProfileData[@"first_name"];
 }
 
 -(void)serverGetRequest {
     
-    NSString *fixedUrl = [NSString stringWithFormat:@"http://www.omdbapi.com/?t=avengers&y=&plot=short&r=json"];
+    NSString *fixedUrl = [NSString stringWithFormat:@"https://api.chariotnow.com/1/user/me"];
+    
     NSURL *url = [NSURL URLWithString:fixedUrl];
+    
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0];
     [request setHTTPMethod:@"GET"];
+    //[request setValue:@"Chariot-Token" forKey:@"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHBpcmUiOjE0MjM4NTY3ODgsInVzZXJpZCI6NDYxOX0.-ooG23KFagd6NJ7--7hJTC--bCXScXr8TDOl57T3JrIgHIO8sB3j7aRC4WdZSvHtYdzuY1mOhb2Cp4tOgAvc_A"];
+    
     NSURLSession *urlSession = [NSURLSession sharedSession];
     NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         
@@ -38,19 +51,19 @@
         NSInteger responseStatusCode = [httpResponse statusCode];
         
         if (responseStatusCode == 200 && data) {
-            
             dispatch_async(dispatch_get_main_queue(), ^(void) {
-                
-                NSDictionary *fetchedData = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                NSLog(@"%@", fetchedData);
+                userProfileData = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                NSLog(@"It Worked!");
+                NSLog(@"%@", userProfileData);
             });
             
         } else {
-            
             NSLog(@"cannot connect to server");
             
         }
+        
     }];
+    
     [dataTask resume];
 }
 
